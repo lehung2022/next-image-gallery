4b8dd4bc742bfaa30067554724bbeb7cfb792cb6
lehung2022 <hung.le.programmer@gmail.com>
2024-02-05 23:32:31 +0700

    changed another thing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,8 @@ function Footer() {
   return (
     <div>
       <h1>Nobody knows that better than I do</h1>
+      <h1>How come this thing does not work? I need to switch branches</h1>
+
     </div>
   )
 }
