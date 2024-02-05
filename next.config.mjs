bcf665eb444e1003637d9abe48125275067cdb53
lehung2022 <hung.le.programmer@gmail.com>
2024-02-05 12:32:43 +0700

    changed the const to function

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = { images: { domains: ['https://drive.google.com/drive/'], formats: ['image/avif', 'image/webp'], }, }
 
 export default nextConfig;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Image from "next/image";
 
-const Header = () => {
+
+function Header() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -114,4 +115,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
