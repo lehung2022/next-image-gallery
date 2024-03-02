bd6857138f374b0ba53d7cdbbedc0eef917ff72d
lehung2022 <hung.le.programmer@gmail.com>
2024-03-02 23:34:29 +0700

    I finished creating header and footer for now. Fix the year by adding date and time for the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
+import Content from "@/components/Content";
 
 export default function Home() {
   return (
     <>
-      <Header />
+      <Navbar />
+      <Content />
       <Footer />
     </>
   );
