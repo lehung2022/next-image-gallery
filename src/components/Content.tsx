0c6364495e7eb375f0c2969e8c05798581010393
lehung2022 <hung.le.programmer@gmail.com>
2024-03-06 01:45:38 +0700

    add fragment in Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Content() {
-  return <div className="h-96 w-80 text-gray-100">Content</div>;
+  return (
+    <>
+      <div className="h-96 w-80 text-gray-100">Content</div>
+    </>
+  );
 }
