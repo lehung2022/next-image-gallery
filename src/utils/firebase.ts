a5abd507767642c020afa4393df41eede251d476
lehung2022 <hung.le.programmer@gmail.com>
2024-03-23 10:31:56 +0700

    added firebase configuration

---
+++ src/utils/firebase.ts
@@ -0,0 +1,29 @@
+// firebaseConfig.ts
+// Remember to install firebase, once we open the project
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+
+type FirebaseConfig = {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+};
+
+const firebaseConfig: FirebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const storage = firebase.storage();
