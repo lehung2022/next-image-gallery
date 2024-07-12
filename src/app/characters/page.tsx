8d9fe198f3d02797a8ef0ad9a504406acde56114
lehung2022 <hung.le.programmer@gmail.com>
2024-07-13 02:02:34 +0700

    changed the layout of the new routers

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Characters() {
-  return (
-    <>
-      <div className="h-20"></div>
-      <div className="h-96 bg-blue-900">Characters</div>
-    </>
-  );
+  return <h1>Hello, Characters Page!</h1>
 }
